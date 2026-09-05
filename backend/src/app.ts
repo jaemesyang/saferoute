ee37f5bc06ab998bc4921a8f381fbfc21ff6f31a
brendon.wang <brendon.wang@gmail.com>
2026-09-05 14:49:43 -0700

    add api/closest

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from 'express';
 import { hotspotRouter } from './modules/hotspots/hotspot.routes.js';
+import {closestRouter} from "./modules/closest/closest.routes.js";
 
 export const app = express();
 
 app.use(express.json());
 
 app.use('/api/hotspots', hotspotRouter);
+app.use('/api/closest', closestRouter)
